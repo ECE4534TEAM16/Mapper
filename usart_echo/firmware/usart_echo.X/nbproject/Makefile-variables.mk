0677e7dc51f48a32dd7903f9936b9d8a2df472f7
tolson4 <tolson4@vt.edu>
2015-10-15 12:55:29 -0400

    USART ECHO
    Uart works here.  Some letters and numbers dont transmit correctly.

---
+++ usart_echo/firmware/usart_echo.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mx795_pim_e16_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mx795_pim_e16_int_dyn=dist/pic32mx795_pim_e16_int_dyn/production
+CND_ARTIFACT_NAME_pic32mx795_pim_e16_int_dyn=usart_echo.X.production.hex
+CND_ARTIFACT_PATH_pic32mx795_pim_e16_int_dyn=dist/pic32mx795_pim_e16_int_dyn/production/usart_echo.X.production.hex
+CND_PACKAGE_DIR_pic32mx795_pim_e16_int_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_int_dyn/package
+CND_PACKAGE_NAME_pic32mx795_pim_e16_int_dyn=usartecho.x.tar
+CND_PACKAGE_PATH_pic32mx795_pim_e16_int_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_int_dyn/package/usartecho.x.tar
+# pic32mx795_pim_e16_poll_dyn configuration
+CND_ARTIFACT_DIR_pic32mx795_pim_e16_poll_dyn=dist/pic32mx795_pim_e16_poll_dyn/production
+CND_ARTIFACT_NAME_pic32mx795_pim_e16_poll_dyn=usart_echo.X.production.hex
+CND_ARTIFACT_PATH_pic32mx795_pim_e16_poll_dyn=dist/pic32mx795_pim_e16_poll_dyn/production/usart_echo.X.production.hex
+CND_PACKAGE_DIR_pic32mx795_pim_e16_poll_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_poll_dyn/package
+CND_PACKAGE_NAME_pic32mx795_pim_e16_poll_dyn=usartecho.x.tar
+CND_PACKAGE_PATH_pic32mx795_pim_e16_poll_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_poll_dyn/package/usartecho.x.tar
